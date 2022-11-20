1d045e66af34e57482f9e869d25a003d8a9c6b21
HogHordGit <hoghord@gmail.com>
2022-11-20 02:53:15 +0200

    add carousel

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,8 +4,8 @@ const slider = new SwipeCarousel({
     containerID: "#carousel",
     slideID: ".slides-body__slide", 
     slidesContainerID: ".slides-body",
-    interval: 2000,
-    isPlaying: true
+    interval: 4000,
+    isPlaying: false
 });
 
 
